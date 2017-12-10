7c00d47b97a3372ec7c0b38b20a3319e30710796
Todd Powell <toddwpowell@gmail.com>
2017-12-09 22:14:16 -0700

    Added responsive and mobile handling of table - resize. Fixed reload/shuffle issue. Renamed "Reload" button to "Shuffle"

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
 let numberArray = Array.apply(0, Array(10000)).map(function (x, y) { return y + 1; });  // [1, 2, 3]
-function reloadNumbers() {
+
+function checkDevice(width) {
+    console.log("width: " + width);
+    let device = null;
+    switch (true) {
+        // case (width < 480):
+        //     console.log("phone: less than 480");
+        case (width < 550):
+        console.log("phone: less than 550");        
+            device = "phone";
+            break;
+        // case (width < 768):
+        //     console.log("mtablet: between 480 and 767");
+        case (width < 800):
+        console.log("mtablet: between 480 and 799");        
+            device = "mTablet";
+            break;
+        case (width < 992):
+            console.log("ltablet: between 800 and 991");
+            device = "lgTablet";
+            break;
+        case (width < 1080):
+            console.log("smDesktop: between 992 and 1079");
+            device = "smDesktop";
+            break;
+        default:
+            console.log("lgDesktop: 1080+");
+            device = "lgDesktop";
+            break;
+    }
+    return device;
+}
+
+function loadNumbers(device, width) {
     $('#numbers-container').css("visibility", "hidden");
     $("#number-list").empty();
-    console.log(numberArray);
+    // console.log(numberArray);
     shuffleArray(numberArray);
-    console.log(numberArray);
+    // console.log(numberArray);
 
-    showNumbers();
+    showNumbers(device, width);
 }
 
 function shuffleArray(array) {
@@ -18,18 +51,36 @@ function shuffleArray(array) {
     }
 }
 
-function showNumbers() {
+function showNumbers(device, width) {
     // $('#numbers-table').remove();
     $('#number-list').find("div").remove();
     //mytable = $('<table></table>').attr({ id: "numbers-table" });
     //myRow = $('</div>');
-	let rows = 500;
-	let cols = 20;
+    let cols = null;
+    switch(device) {
+        case "phone":
+            cols = Math.floor(width/50)-1; // There are too many screen sizes on small phones. This will adjust.
+            break;
+        case "mTablet":
+            cols = 10;
+            break;
+        case "lgTablet":
+            cols = 15;
+            break;
+        case "smDesktop":
+            cols = 15;
+            break;
+        default:
+            cols = 20;
+            break;
+    }
+    console.log("cols: " + cols);
+    let rows = Math.ceil(10000/cols);
+    console.log("rows: " + rows);
     let tr = [];
     let idx = 0;
 	for (let i = 0; i < rows; i++) {
 		// let row = $('<tr></tr>').attr({ class: ["class1", "class2", "class3"].join(' ') }).appendTo(mytable);
-        console.log("--- row: " + i);
         let row = $('<div></div>').appendTo("#number-list");
         if (i % 2 === 0) {
             row.addClass("odd");
@@ -37,7 +88,6 @@ function showNumbers() {
             row.addClass("even");
         }
         for (let j = 0; j < cols; j++) {
-            console.log("    div: " + j);
             $('<td></td>').text(numberArray[idx]).appendTo(row); 
             idx++;
 		}
